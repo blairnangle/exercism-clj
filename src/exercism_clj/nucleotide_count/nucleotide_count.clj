bbab0e1cf0f702ab9d5859cb1ea0ed3d60b67064
Blair Nangle <hi@blairnangle.com>
2022-10-29 17:23:20 +0100

    Complete "Nucleotide Count"

---
+++ src/exercism_clj/nucleotide_count/nucleotide_count.clj
@@ -0,0 +1,14 @@
+(ns exercism-clj.nucleotide-count.nucleotide-count
+  (:require [clojure.string :as str]))
+
+(defn count-of-nucleotide-in-strand
+  [nucleotide strand]
+  (if (not (.contains '(\A \T \C \G) nucleotide)) (throw (Throwable. "error"))
+                                           (count (filter #(= % (str nucleotide)) (str/split strand #""))))
+  )
+
+
+(defn nucleotide-counts
+  [strand]
+  (into {} (map #(assoc {} % (count-of-nucleotide-in-strand % strand)) [\A \T \C \G]))
+  )
