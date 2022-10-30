03b081a05dfb031fa5e55c0cda2a089357730886
Blair Nangle <hi@blairnangle.com>
2022-10-30 10:37:00 +0000

    In "Nucleotide Count", use native Clojure `contains?`
    - Exercism was complaining about use of Java interop method

--- src/exercism_clj/nucleotide_count/nucleotide_count.clj
+++ src/exercism_clj/nucleotide_count/nucleotide_count.clj
@@ -3,12 +3,19 @@
 
 (defn count-of-nucleotide-in-strand
   [nucleotide strand]
-  (if (not (.contains '(\A \T \C \G) nucleotide)) (throw (Throwable. "error"))
-                                           (count (filter #(= % (str nucleotide)) (str/split strand #""))))
+  (if
+    (not
+      (contains? #{\A \T \C \G} nucleotide)
+      ) (throw (Throwable. "error"))
+        (count (filter #(= % (str nucleotide)) (str/split strand #""))))
   )
 
 
 (defn nucleotide-counts
   [strand]
-  (into {} (map #(assoc {} % (count-of-nucleotide-in-strand % strand)) [\A \T \C \G]))
+  (into {}
+        (map #(assoc {} %
+                        (count-of-nucleotide-in-strand % strand)) [\A \T \C \G]
+             )
+        )
   )
